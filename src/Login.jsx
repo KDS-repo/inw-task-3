1dab39f9cdba0bcf117b507011e5b7d8602a5325
KDS <kohany.d@gmail.com>
2025-12-29 04:39:00 +0300

    feat: jsx skeleton for login page built

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,35 @@
-function Login(){
-    return(
-        <div>Hello World</div>
+import { Link } from 'react-router-dom'
+
+function Login() {
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('Login attempt!')
+        navigate('/')
+    }
+    return (
+        <div>
+            <h1>
+                <Link to="/">Home</Link>
+            </h1>
+            <h2>Log in to your account</h2>
+            <form onSubmit={handleSubmit} className="login-form">
+                <input
+                    type="email"
+                    placeholder="Email"
+                    //value=
+                    //onChange=
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    //value=
+                    //onChange=
+                    required
+                />
+                <button type="submit">Log In</button>
+            </form>
+        </div>
     )
 }
 
