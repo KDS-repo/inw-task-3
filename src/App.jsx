87c9797639da3caf4feb4d1e6cbf03508dfe580a
KDS <kohany.d@gmail.com>
2025-12-29 04:15:32 +0300

    feat: empty login page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
+import Login from "./Login.jsx"
 import Page from "./Page.jsx"
 
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path='/' element={<Page />}>
       </Route>
     </Routes>
