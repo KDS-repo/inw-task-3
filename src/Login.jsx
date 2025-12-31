5b1c7a096256fb036c7db9f23992983a4ecef8ff
KDS <kohany.d@gmail.com>
2025-12-31 18:28:34 +0300

    style: css style added to login
    fixing import necessary

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,33 +1,37 @@
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import './login.css';
 
 function Login() {
+    const navigate = useNavigate()
+
     const handleSubmit = (event) => {
         event.preventDefault()
         console.log('Login attempt!')
         navigate('/')
     }
     return (
-        <div>
-            <h1>
+        <div className='login'>
+            <h1 className='login__header'>
                 <Link to="/">Home</Link>
             </h1>
-            <h2>Log in to your account</h2>
+            <h2 className='login__subheader'>Log in to your account</h2>
             <form onSubmit={handleSubmit} className="login-form">
-                <input
+                <input className='login__email'
                     type="email"
                     placeholder="Email"
                     //value=
                     //onChange=
                     required
                 />
-                <input
+                <input className='login__password'
                     type="password"
                     placeholder="Password"
                     //value=
                     //onChange=
                     required
                 />
-                <button type="submit">Log In</button>
+                <button type="submit" className='login__submit'>Log In</button>
             </form>
         </div>
     )
