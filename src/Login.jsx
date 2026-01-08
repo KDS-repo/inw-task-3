5f97ec32d3d4dd6874857ae4f41b5fb958a6b42c
KDS <kohany.d@gmail.com>
2026-01-08 17:46:57 +0300

    feat: authentication token received from dummyjson

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,22 +2,37 @@ import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react';
 import './login.css';
+import { useLoginMutation } from './redux/authAPI'
 
 function Login() {
     const navigate = useNavigate()
-    //const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
     const [isValid, setIsValid] = useState(true)
+    // Unlike query, a mutation returns an array with the function being first and the state inside an object that comes second
+    // I'm destructuring both
+    const [login, { isLoading, error }] = useLoginMutation();
 
-    const checkEmail = (event) => {
+    const usernameUpdate = (event) => {
         // Emaiil validation regex
-        const val = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(event.target.value)
-        setIsValid(val)
+        //const val = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(event.target.value)
+        //setIsValid(val)
+        setUsername(event.target.value)
+    }
+    const passwordUpdate = (event) => {
+        setPassword(event.target.value)
     }
     const handleSubmit = (event) => {
+        //if(!isValid) return
         event.preventDefault()
         console.log('Login attempt!')
-        navigate('/')
+        login({username: username, password: password})
+            .unwrap() // Necessary for RTK
+            .then((result) => localStorage.setItem('token', result.accessToken)) // Move to RTK later
+            .then(() => navigate('/'))
+            .catch((error) => console.log("An error occured:", error)) // Should throw an error if credentials are wrong
     }
+
     return (
         <div className='login'>
             <h1 className='login__header'>
@@ -25,18 +40,19 @@ function Login() {
             </h1>
             <h2 className='login__subheader'>Log in to your account</h2>
             <form onSubmit={handleSubmit} className="login-form">
-                <input className={`login__email ${!isValid && 'login_incorrect'}`}
-                    type="email"
-                    placeholder="Email"
-                    //value={email}
-                    onChange={checkEmail}
+                <input 
+                    className={`login__email ${!isValid && 'login_incorrect'}`}
+                    type="text"
+                    placeholder="Username"
+                    //value=
+                    onChange={usernameUpdate}
                     required
                 />
                 <input className='login__password'
                     type="password"
                     placeholder="Password"
                     //value=
-                    //onChange=
+                    onChange={passwordUpdate}
                     required
                 />
                 <button type="submit" className='login__submit'>Log In</button>
--- src/redux/storage.js
+++ src/redux/storage.js
@@ -1,15 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import articleReducer from './articleSlice';
-import { articlesAPI } from './reduxAPI';
+import { articlesAPI } from './articlesAPI';
+import { authAPI } from './authAPI';
 
 const store = configureStore({
     reducer: {
         // Check for path correctness, there are similar names
         articlesApi: articlesAPI.reducer,
-        articles: articleReducer
+        articles: articleReducer,
+        authApi: authAPI.reducer
     },
     // Default middleware is generated automatically
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articlesAPI.middleware)
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware()
+            .concat(articlesAPI.middleware)
+            .concat(authAPI.middleware)
 })
 
 export default store
