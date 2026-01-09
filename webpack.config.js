35304646e0ce7a1783abf241bc0e4374a40243a9
KDS <kohany.d@gmail.com>
2026-01-09 17:31:45 +0300

    attempt: raw config, throws error with import and export

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/main.jsx',
+    output: {
+      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      publicPath: '/'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|mjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/images/[name][ext]'
+          }
+        },
+        {
+          test: /\.svg$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/svgs/[name][ext]'
+          }
+        }
+      ]
+    },
+
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html'
+      })
+    ],
+
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public')
+      },
+      port: 5173,
+      open: true,
+      hot: true,
+      historyApiFallback: true // Important for React Router
+    },
+
+    devtool: isProduction ? 'source-map' : 'eval-source-map'
+  };
+};
